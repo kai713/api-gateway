b916284c97677f922a8f98637773473e04c4a359
kai713 <kairgaliev713@gmail.com>
2024-11-24 21:03:31 +0500

    added swagger ui, docs, and aggregated with other services, added dependency openAPI doc, ui, changed classes: Routes java, SecurityConfig (just deleted not need annotation)

--- src/main/java/com/kairgaliyev/microservices/gateway/config/SecurityConfig.java
+++ src/main/java/com/kairgaliyev/microservices/gateway/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.kairgaliyev.microservices.gateway.config;
 
-import com.sun.net.httpserver.HttpServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -10,9 +9,21 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+    //this url which need be authenticated (free urls), so below code that by requestMatchers check
+    // and permit (разрешать) all our urls
+    // code
+    //    authorize
+    //   .requestMatchers(freeResourceUrls)
+    //    .permitAll()
+    private final String[] freeResourceUrls = {"/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/aggregate/**"};
+
+    //By this: authorize -> authorize.anyRequest().authenticated(), written that every request must be authenticated
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.authorizeRequests(authorize -> authorize.anyRequest().authenticated())
+        return httpSecurity.authorizeRequests(authorize -> authorize
+                        .requestMatchers(freeResourceUrls)
+                        .permitAll()
+                        .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
     }
